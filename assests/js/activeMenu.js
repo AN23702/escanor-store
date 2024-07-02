6cc65e79fbbb6afc7d93fed36a9352057db5d00e
AN23702 <161464637+AN23702@users.noreply.github.com>
2024-07-02 14:04:35 +0700

    update

--- assests/js/activeMenu.js
+++ assests/js/activeMenu.js
@@ -1,28 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const setActiveMenu = (selector) => {
-        const menuItems = document.querySelectorAll(selector);
+    const removeActiveClasses = (menuItems) => {
+      menuItems.forEach((item) => item.classList.remove("active"));
+    };
+  
+    const setActiveMenu = (selector, defaultActiveIndex = 0) => {
+      const menuItems = document.querySelectorAll(selector);
+  
+      const activeIndex = localStorage.getItem("activeMenuItemIndex");
+      if (activeIndex !== null) {
 
+        const activeElement = menuItems[activeIndex];
+        if (activeElement) {
+          removeActiveClasses(menuItems);
+          activeElement.classList.add("active");
+        }
+      } else {
+        menuItems[defaultActiveIndex].classList.add("active");
 
-        menuItems.forEach((item,index) =>{
-
-            item.addEventListener("click", (event) =>{
-                event.preventDefault();
-                //Xoa cac active cu
-                removeActiveClasses(menuItems);
-
-                //Them active moi
-                item.classList.add("active");
-            });
+      }
+  
+      menuItems.forEach((item, index) => {
+        item.addEventListener("click", () => {
+          removeActiveClasses(menuItems);
+          item.classList.add("active");
+  
+          localStorage.setItem("activeMenuItemIndex", index);
         });
-
-        const removeActiveClasses = (menuItems) => {
-            console.log(menuItems);
-            menuItems.forEach((value) => {
-                value.classList.remove("active");
-            });
-        }      
-    }
-
-    setActiveMenu(".menu-main li");
-
-});
+      });
+    };
+  
+    setActiveMenu(".menu-main li", 0);
+  });
+  
