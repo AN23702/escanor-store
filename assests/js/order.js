498cbd067404b2769a0b6b3d585ab2d82883de6d
AN23702 <161464637+AN23702@users.noreply.github.com>
2024-07-13 00:37:08 +0700

    update

--- assests/js/order.js
+++ assests/js/order.js
@@ -19,79 +19,6 @@ $('.logo-banner').owlCarousel({
     }
 })
 
-// Banner
-$('.fix-banner').owlCarousel({
-  loop:true,
-  // margin:15,
-  nav:false,
-  dots:false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
-})
-
-// Sale products
-$('.sale-products-banner').owlCarousel({
-  loop:true,
-  dots:false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  margin:10,
-  nav:false,
-  responsive:{
-      0:{
-          items:4
-      },
-      600:{
-          items:4
-      },
-      1000:{
-          items:4
-      },
-      1500:{
-        items:4
-    }
-  }
-})
-
-/* Title products sale */
-var textWrapper = document.querySelector('.title-sale-products .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.title-sale-products .letter',
-    scale: [0.3,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 600,
-    delay: (el, i) => 70 * (i+1)
-  }).add({
-    targets: '.title-sale-products .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700,
-    offset: '-=875',
-    delay: (el, i, l) => 80 * (l - i)
-  }).add({
-    targets: '.title-sale-products',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
 
 // Header main
 const headerMain = document.querySelector(".header-main")
@@ -152,19 +79,4 @@ closeChat.addEventListener('click', function() {
   }
 
   chatBox.classList.toggle('show');
-});
-
-// Cart box
-const cartButton = document.querySelector('.cart');
-const closeCartButton = document.querySelector('.close-cart-box');
-const cartBox = document.querySelector('.cart-box');
-
-cartButton.addEventListener('click', () => {
-    cartBox.style.display = ('block');
-    cartBox.style.animation = ('fadeInRight .3s ease-in-out')
-});
-
-closeCartButton.addEventListener('click', () => {
-    cartBox.style.display = ('none');
-    cartBox.style.animation = ('fadeOutRight .3s ease-in-out')
 });
